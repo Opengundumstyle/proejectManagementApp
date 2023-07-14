6698cfe6a711d56c11fd083583da115a76d6fd5d
Zhile Lin <zhlin04@gmail.com>
2023-07-14 00:03:20 -0700

    page restructured

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 
 import Header from "./components/Header";
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import {ApolloProvider,ApolloClient,InMemoryCache} from '@apollo/client'
-import Clients from "./components/Clients";
-import AddClientModal from "./components/AddClientModal";
-import Projects from "./components/Projects";
+import Home from "./pages/Home";
 
 const cache = new InMemoryCache({
    typePolicies:{
@@ -34,12 +33,14 @@ function App() {
   return (
     <>
     <ApolloProvider client={client}>
-     <Header/>
-    <div className="container">
-           <AddClientModal/>
-           <Projects/>
-          <Clients/>
-    </div>
+      <Router>
+        <Header/>
+        <div className="container">
+              <Routes>
+                   <Route path="/" element={<Home/>}/>
+              </Routes>
+        </div>
+    </Router>
     </ApolloProvider>
     </>
   );
