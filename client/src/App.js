bf4dff4380f6e08e181aae4ba3bea4a2a2cafb25
Zhile Lin <zhlin04@gmail.com>
2023-07-14 00:31:52 -0700

    project query

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import {ApolloProvider,ApolloClient,InMemoryCache} from '@apollo/client'
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
 
 const cache = new InMemoryCache({
    typePolicies:{
@@ -39,6 +40,7 @@ function App() {
         <div className="container">
               <Routes>
                    <Route path="/" element={<Home/>}/>
+                   <Route path="projects/:id" element={<Project/>}/>
                    <Route path="*" element={<NotFound/>}/>
               </Routes>
         </div>
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,8 +1,21 @@
+import { Link,useParams } from "react-router-dom"
+import Spinner from "../components/Spinner"
+import { useQuery } from "@apollo/client"
+import { GET_PROJECT } from "../queries/projectQueries"
 
 const Project = () => {
+
+  const {id} = useParams()
+  const {loading,error,data} = useQuery(GET_PROJECT,{
+      variables:{id}
+  })
+
+  if(loading) return <Spinner/>
+  if(error) return <p>Something went wrong</p>
+
   return (
    <>
-     
+     dam
    </>
   )
 }
