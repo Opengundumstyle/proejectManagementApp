0860abfecc7cd1ff60b8fb6d1c080c1582f8fa09
Zhile Lin <zhlin04@gmail.com>
2023-08-03 16:52:44 -0700

    create project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import AddClientModal from '../components/AddClientModal'
+import AddProjectModal from '../components/addProjectModal'
 import Projects from '../components/Projects'
 import Clients from '../components/Clients'
+
 
 const Home = () => {
   return (
     <>
       <div className="d-flex gap-3 mb-4">
       <AddClientModal/>
+      <AddProjectModal/>
       </div>
        <Projects/>
          <hr />
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -2,6 +2,7 @@ import { Link,useParams } from "react-router-dom"
 import Spinner from "../components/Spinner"
 import { useQuery } from "@apollo/client"
 import { GET_PROJECT } from "../queries/projectQueries"
+import ClientInfo from "../components/ClientInfo"
 
 const Project = () => {
 
@@ -15,7 +16,22 @@ const Project = () => {
 
   return (
    <>
-     dam
+     {
+        !loading && !error &&(
+
+            <div className="mx-auto w-75 card p-5">
+                   <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">
+                       Back
+                   </Link>
+                   <h1>{data.project.name}</h1>
+                   <p>{data.project.description}</p>
+                   <h5 className="mt-5">Project Status</h5>
+                   <p className="lead">{data.project.status}</p>
+
+                  <ClientInfo client={data.project.client}/>
+            </div>
+        )
+     }
    </>
   )
 }
